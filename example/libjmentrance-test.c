6c2d2bb44d0599e7d5f6da4f36b04fdbf2f5e7f4
Ahmad <amarullz@gmail.com>
2024-08-06 08:50:31 +0700

    Add JMJ example

--- example/libjmentrance-test.c
+++ example/libjmentrance-test.c
@@ -41,16 +41,25 @@ const char* jmentrance_jmj_pubkey =
 /* Cluster-ID Key untuk JMJ */
 const char* jmentrance_jmj_cluster_id = "ba5cee4e";
 
-int main() {
+/* Print Info */
+void print_info(const char* title, const char* uid, const char* enc,
+                const char* dec, int ecd) {
+  printf(
+      "%s\n"
+      "  UUID      : %s\n"
+      "  ENCRYPTED : %s\n"
+      "  DECRYPTED : %s\n"
+      "  ERR-CODE  : %i\n\n",
+      title, uid, enc, dec, ecd);
+}
+
+/* Test Data JJS */
+void test_jjs() {
   int ret;
 
   /* JJS */
-  //   const char * pubkey =jmentrance_jjs_pubkey;
-  //   const char * clusterid =jmentrance_jjs_cluster_id;
-
-  /* JMJ */
-  const char* pubkey = jmentrance_jmj_pubkey;
-  const char* clusterid = jmentrance_jmj_cluster_id;
+  const char* pubkey = jmentrance_jjs_pubkey;
+  const char* clusterid = jmentrance_jjs_cluster_id;
 
   /*
   TEST BCA/DINAS (BELUM EXIT)
@@ -69,13 +78,7 @@ int main() {
   const char* data_bca = "05A4B618A982AA55D61C1284684923E7";
   char out_bca[45] = {0};
   ret = jmentrance_decrypt(pubkey, clusterid, sn_bca, data_bca, out_bca, 45);
-  printf(
-      "TEST BCA/DINAS\n"
-      "UUID      : %s\n"
-      "ENCRYPTED : %s\n"
-      "DECRYPTED : %s\n"
-      "ERR-CODE  : %i\n\n",
-      sn_bca, data_bca, out_bca, ret);
+  print_info("TEST BCA/DINAS", sn_bca, data_bca, out_bca, ret);
 
   /*
   TEST ETOLL NON BCA (BELUM EXIT)
@@ -95,13 +98,8 @@ int main() {
   char out_etoll1[45] = {0};
   ret = jmentrance_decrypt(pubkey, clusterid, sn_etoll1, data_etoll1,
                            out_etoll1, 45);
-  printf(
-      "TEST ETOLL NON BCA (BELUM EXIT)\n"
-      "UUID      : %s\n"
-      "ENCRYPTED : %s\n"
-      "DECRYPTED : %s\n"
-      "ERR-CODE  : %i\n\n",
-      sn_etoll1, data_etoll1, out_etoll1, ret);
+  print_info("TEST ETOLL NON BCA (BELUM EXIT)", sn_etoll1, data_etoll1,
+             out_etoll1, ret);
 
   /*
   TEST ETOLL NON BCA (SUDAH EXIT)
@@ -115,20 +113,118 @@ int main() {
   Checksum         : 0
   SN               : 7546000012122780
   ENCRYPTED-DATA   : 615870B6B93F44ABB07EEADCB581CBC6FFFFFFFFFFFF
-
-  *** Panjang encrypted data berisi lebih dari 32hex.
   */
   const char* sn_etoll2 = "7546000012122780"; /* Non-BCA Gunaan SN */
   const char* data_etoll2 = "615870B6B93F44ABB07EEADCB581CBC6FFFFFFFFFFFF";
   char out_etoll2[45] = {0};
   ret = jmentrance_decrypt(pubkey, clusterid, sn_etoll2, data_etoll2,
                            out_etoll2, 45);
-  printf(
-      "TEST ETOLL NON BCA (SUDAH EXIT)\n"
-      "UUID      : %s\n"
-      "ENCRYPTED : %s\n"
-      "DECRYPTED : %s\n"
-      "ERR-CODE  : %i\n\n",
-      sn_etoll2, data_etoll2, out_etoll2, ret);
+  print_info("TEST ETOLL NON BCA (SUDAH EXIT)", sn_etoll2, data_etoll2,
+             out_etoll2, ret);
+}
+
+/* Test Data JMJ */
+void test_jmj() {
+  int ret;
+
+  /* JMJ */
+  const char* pubkey = jmentrance_jmj_pubkey;
+  const char* clusterid = jmentrance_jmj_cluster_id;
+
+  /* Variabel */
+  char* sn;
+  char* enc;
+  char out[45] = {0};
+
+  /*
+    --------------------------------------------
+    CB / GERBANG / GARDU  : 45 / 30 / 01
+    GOLONGAN              : 1
+    TGL_ENTRANCE          : 06/08/2024 08:18:14
+    PETUGAS               : 151498
+    IID                   : 0003
+    STAT / JENIS / DIR    : 00 / 00 / 00
+    GERBANG EXIT          : 00
+    UID/SN                : 6032982864461171
+    ____________________________________________
+    METODA BAYAR : MANDIRI
+    SN           : 6032982864461171
+    DATA ENCRYPT : 7813822AEBB9A40540A6B9E87306F195FFFFFFFFFFFF
+  */
+  sn = "6032982864461171";
+  enc = "7813822AEBB9A40540A6B9E87306F195FFFFFFFFFFFF";
+  ret = jmentrance_decrypt(pubkey, clusterid, sn, enc, out, 45);
+  print_info("JJS MANDIRI", sn, enc, out, ret);
+
+  /*
+    --------------------------------------------
+    CB / GERBANG / GARDU  : 45 / 30 / 01
+    GOLONGAN              : 2
+    TGL_ENTRANCE          : 06/08/2024 08:18:30
+    PETUGAS               : 151498
+    IID                   : 0003
+    STAT / JENIS / DIR    : 00 / 00 / 00
+    GERBANG EXIT          : 00
+    UID/SN                : 15CC8081
+    ____________________________________________
+    METODA BAYAR : BCA
+    UID          : 15CC8081
+    DATA ENCRYPT : 71634CB6147414803B8E57A42E26FEF2
+  */
+  sn = "15CC8081";
+  enc = "71634CB6147414803B8E57A42E26FEF2";
+  ret = jmentrance_decrypt(pubkey, clusterid, sn, enc, out, 45);
+  print_info("JJS BCA", sn, enc, out, ret);
+
+  /*
+    --------------------------------------------
+    CB / GERBANG / GARDU  : 45 / 30 / 01
+    GOLONGAN              : 3
+    TGL_ENTRANCE          : 06/08/2024 08:18:43
+    PETUGAS               : 151498
+    IID                   : 0003
+    STAT / JENIS / DIR    : 00 / 00 / 00
+    GERBANG EXIT          : 00
+    UID/SN                : 6013500418244902
+    ____________________________________________
+    METODA BAYAR : BRI
+    SN           : 6013500418244902
+    DATA ENCRYPT : B31818BEB247BDB46CF4AD672FD0973DFFFFFFFFFFFF
+  */
+  sn = "6013500418244902";
+  enc = "B31818BEB247BDB46CF4AD672FD0973DFFFFFFFFFFFF";
+  ret = jmentrance_decrypt(pubkey, clusterid, sn, enc, out, 45);
+  print_info("JJS BRI", sn, enc, out, ret);
+
+  /*
+    --------------------------------------------
+    CB / GERBANG / GARDU  : 45 / 30 / 01
+    GOLONGAN              : 4
+    TGL_ENTRANCE          : 06/08/2024 08:18:53
+    PETUGAS               : 151498
+    IID                   : 0003
+    STAT / JENIS / DIR    : 00 / 00 / 00
+    GERBANG EXIT          : 00
+    UID/SN                : 7546130008699754
+    ____________________________________________
+    METODA BAYAR : BNI
+    SN           : 7546130008699754
+    DATA ENCRYPT : 92BAAF2F1BF8226473031612D12362B4FFFFFFFFFFFF
+  */
+  sn = "7546130008699754";
+  enc = "92BAAF2F1BF8226473031612D12362B4FFFFFFFFFFFF";
+  ret = jmentrance_decrypt(pubkey, clusterid, sn, enc, out, 45);
+  print_info("JJS BNI", sn, enc, out, ret);
+}
+
+int main() {
+  printf("TEST DATA JJS\n");
+  printf("=============\n");
+  test_jjs();
+
+  printf("\n\n");
+  printf("TEST DATA JMJ\n");
+  printf("=============\n");
+  test_jmj();
   return 0;
 }
